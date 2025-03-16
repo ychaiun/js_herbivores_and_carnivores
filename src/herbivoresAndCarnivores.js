8594a0688fdaf5468ccecb67270f59269dc8d19b
Yurii Chain <yuriichaiun@gmail.com>
2025-03-16 18:55:02 +0200

    fix replace filter method from Animal to bite method

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -9,10 +9,6 @@ class Animal {
 
     Animal.alive.push(this);
   }
-
-  deadAnimal() {
-    Animal.alive = Animal.alive.filter((animal) => animal !== this);
-  }
 }
 
 class Herbivore extends Animal {
@@ -33,12 +29,14 @@ class Carnivore extends Animal {
   }
 
   bite(animal) {
-    if (animal instanceof Herbivore && !animal.hidden) {
-      animal.health -= 50;
+    if (!(animal instanceof Herbivore) || animal.hidden) {
+      return;
     }
 
+    animal.health -= 50;
+
     if (animal.health <= 0) {
-      animal.deadAnimal();
+      Animal.alive = Animal.alive.filter((beast) => beast !== animal);
     }
   }
 }
